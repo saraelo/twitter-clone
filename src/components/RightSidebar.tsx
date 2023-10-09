2df816faed6a916d14d5a6fe982f2e7594e2ad13
Sara Loera <iamsaraelo@icloud.com>
2023-10-09 10:36:05 -0700

    Integrate shadcn library into the project

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -2,14 +2,14 @@ import { BsSearch } from 'react-icons/bs';
 
 const RightSidebar = () => {
   return (
-    <section className="sticky top-2 w-[30%] flex flex-col items-stretch h-screen px-6 mt-2 ">
+    <section className="sticky top-2 w-[30%] h-screen flex flex-col items-stretch px-6 mt-2">
       <div>
         <div className="relative w-full h-full group">
           <input
             id="searchBox"
             type="text"
             placeholder="Search Twitter"
-            className="outline-none peer focus:border-primary focus:border bg-neutral-900/90 w-full h-full rounded-xl py-4 pl-14 pr-4"
+            className="outline-none peer w-full h-full rounded-xl py-4 pl-14 pr-4 bg-neutral-900/90 focus:border-primary focus:border"
           />
           <label
             htmlFor="searchBox"
@@ -19,37 +19,36 @@ const RightSidebar = () => {
           </label>
         </div>
       </div>
-      <div className="flex flex-col rounded-xl bg-neutral-900 my-4">
-        <h3 className="font-bold text-xl my-4 px-4">Trends for you</h3>
+      <div className="flex flex-col w-full bg-neutral-900 my-4 rounded-xl">
+        <h3 className="font-bold text-xl px-4 my-4">Trends for you</h3>
         <div>
           {Array.from({ length: 5 }).map((_, i) => (
             <div
               key={i}
-              className="hover:bg-white/10 p-4 last:rounded-b-xl transition duration-200"
+              className="p-4 last:rounded-b-xl transition duration-200 hover:bg-white/10"
             >
-              <div className="font-bold text-lg ">#trending {i + 1}</div>
+              <div className="font-bold text-lg">#trending {i + 1}</div>
               <div className="text-xs text-neutral-400">35.4k</div>
             </div>
           ))}
         </div>
       </div>
-      <div className="flex flex-col rounded-xl bg-neutral-900 my-4">
-        <h3 className="font-bold text-xl my-4 px-4">Who to follow</h3>
+      <div className="flex flex-col w-full bg-neutral-900 my-4 rounded-xl">
+        <h3 className="font-bold text-xl px-4 my-4">Who to follow</h3>
         <div>
           {Array.from({ length: 5 }).map((_, i) => (
             <div
               key={i}
-              className="hover:bg-white/10 p-4 flex justify-between items-center last:rounded-b-xl transition duration-200"
+              className="flex justify-between items-center p-4 last:rounded-b-xl transition duration-200 hover:bg-white/10"
             >
               <div className="flex items-center space-x-2">
-                <div className="w-10 h-10 bg-neutral-600 rounded-full flex-none"></div>
+                <div className="w-10 h-10 flex-none bg-neutral-600 rounded-full"></div>
                 <div className="flex flex-col">
                   <div className="font-bold text-white">User User</div>
-                  <div className="text-gray-500 text-xs">@useruser1111</div>
+                  <div className="text-xs text-gray-500">@useruser1111</div>
                 </div>
               </div>
-
-              <button className="rounded-full px-6 py-2 bg-white text-neutral-950">
+              <button className="px-6 py-2 bg-white text-neutral-950 rounded-full">
                 Follow
               </button>
             </div>
